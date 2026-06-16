d00551104fd25de82ea62b745cb4d9ec3f071f3b
Shaurya Sharma <65916881+CreativeCoder-GitHub@users.noreply.github.com>
2026-06-16 09:25:19 -0400

    Upload the majority of the assets

---
+++ assets/level/platformer-tileset.tsx
@@ -0,0 +1,447 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="platformer-tileset" tilewidth="32" tileheight="32" tilecount="84" columns="12">
+ <image source="tileset_32x32(new).png" width="384" height="254"/>
+ <tile id="0">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="hitbox" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="hitbox" type="bool" value="false"/>
+   <property name="is_finish" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="hitbox" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="hitbox" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="hitbox" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="hitbox" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="hitbox" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="67">
+  <properties>
+   <property name="danger" type="bool" value="true"/>
+   <property name="hitbox" type="bool" value="false"/>
+  </properties>
+  <animation>
+   <frame tileid="66" duration="250"/>
+   <frame tileid="67" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="68">
+  <properties>
+   <property name="hitbox" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="69">
+  <properties>
+   <property name="hitbox" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="hitbox" type="bool" value="false"/>
+   <property name="is_coin" type="bool" value="true"/>
+  </properties>
+  <animation>
+   <frame tileid="72" duration="250"/>
+   <frame tileid="73" duration="250"/>
+   <frame tileid="74" duration="250"/>
+   <frame tileid="75" duration="250"/>
+   <frame tileid="76" duration="250"/>
+   <frame tileid="77" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="78">
+  <properties>
+   <property name="is_enemy" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="79">
+  <properties>
+   <property name="is_boss" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <wangsets>
+  <wangset name="Grass" type="corner" tile="-1">
+   <wangcolor name="Grass" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="3" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="4" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="5" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="12" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="13" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="14" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="15" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="17" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="24" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="25" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="26" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="27" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="28" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="29" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="42" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="43" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="44" wangid="0,1,0,1,0,0,0,1"/>
+  </wangset>
+  <wangset name="Stone" type="corner" tile="-1">
+   <wangcolor name="Stone" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="6" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="7" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="8" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="9" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="10" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="11" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="18" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="19" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="20" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="21" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="23" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="30" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="31" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="32" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="33" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="34" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="35" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="54" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="55" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="56" wangid="0,1,0,1,0,0,0,1"/>
+  </wangset>
+  <wangset name="Dirt" type="corner" tile="-1">
+   <wangcolor name="Dirt" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="36" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="37" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="38" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="39" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="40" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="41" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="48" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="49" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="50" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="51" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="53" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="60" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="61" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="62" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="63" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="64" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="65" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
